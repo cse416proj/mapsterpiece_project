24e578c75273620732edab3aa5e7ac5785c07b28
20shiy <shiying6120@gmail.com>
2023-11-03 13:47:05 -0400

    testing frontend workflow:

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/A platform for map lovers/i);
-  expect(linkElement).toBeInTheDocument();
+  const descriptionElement = screen.getByText(/A platform for map lovers/i);
+  expect(descriptionElement).toBeInTheDocument();
 });
