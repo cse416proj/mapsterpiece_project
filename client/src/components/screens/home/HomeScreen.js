480c6f2a7e1c39cb2c1afa4b298a87db326d5c7c
ChoiYingY <ycying1226@gmail.com>
2023-11-03 23:56:27 -0400

    created signin & login screen, restructured files & folders location

--- client/src/components/screens/home/HomeScreen.js
+++ client/src/components/screens/home/HomeScreen.js
@@ -1,15 +1,15 @@
 import { Box } from '@mui/material';
 
 import Hero from './Hero';
-import Copyright from './Copyright';
+import Copyright from '../../Copyright';
 
-function Home(){
+function HomeScreen(){
     return(
-        <Box id='content'>
+        <Box className='content'>
             <Hero/>
             <Copyright/>
         </Box>
     )
 }
 
-export default Home;
+export default HomeScreen;
