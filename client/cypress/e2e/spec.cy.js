7395b79806b81c501114c3900f9b75dda8885225
Zian Shang <zian.shang@stonybrook.edu>
2023-11-04 23:31:17 -0400

    cypress frontend test case adjustment

--- client/cypress/e2e/spec.cy.js
+++ client/cypress/e2e/spec.cy.js
@@ -1,9 +1,18 @@
 describe('template spec', () => {
   it('register user', () => {
-    cy.visit('/')
+    cy.visit('/');
 
     /* ==== Generated with Cypress Studio ==== */
     cy.get('#prompt > .flex-row > :nth-child(1)').click();
+    cy.get('#\\:r1\\:').clear('Mappy');
+    cy.get('#\\:r1\\:').type('Mappy');
+    cy.get('#\\:r3\\:').clear('mappy1234@gmail.com');
+    cy.get('#\\:r3\\:').type('mappy1234@gmail.com');
+    cy.get('#\\:r5\\:').clear('mappy@');
+    cy.get('#\\:r5\\:').type('mappy@');
+    cy.get('#\\:r7\\:').clear('mappy@');
+    cy.get('#\\:r7\\:').type('mappy@');
+    cy.get('#filled-btn').click();
     /* ==== End Cypress Studio ==== */
   })
 
@@ -13,17 +22,21 @@ describe('template spec', () => {
     cy.visit('/');
     /* ==== Generated with Cypress Studio ==== */
     cy.get('[style="margin-left: 2.5vw;"]').click();
+    cy.get('#\\:r1\\:').clear('mappy1234@gmail.com');
+    cy.get('#\\:r1\\:').type('mappy1234@gmail.com');
+    cy.get('#\\:r3\\:').clear('mappy@');
+    cy.get('#\\:r3\\:').type('mappy@');
+    cy.get('#filled-btn').click();
     /* ==== End Cypress Studio ==== */
   })
 
   it('dropdown menu test', ()=>{
     cy.visit('/');
     /* ==== Generated with Cypress Studio ==== */
-    cy.get('path').click();
+    cy.get('[data-testid="PersonIcon"]').click();
     cy.get('.Mui-focusVisible').click();
     cy.get('[data-testid="PersonIcon"]').click();
     cy.get('.MuiList-root > [tabindex="-1"]').click();
     /* ==== End Cypress Studio ==== */
   })
-
 })
