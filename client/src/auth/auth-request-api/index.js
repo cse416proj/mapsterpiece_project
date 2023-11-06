f580af988b1334913376630ed71ea2ccd1af1619
Zian Shang <zian.shang@stonybrook.edu>
2023-11-05 23:24:09 -0500

    build run test bug fix attempt

--- client/src/auth/auth-request-api/index.js
+++ client/src/auth/auth-request-api/index.js
@@ -1,4 +1,5 @@
-import axios from 'axios'
+import axios from 'axios';
+// const {axios} = require("axios");
 axios.defaults.withCredentials = true;
 const api = axios.create({
     baseURL: 'http://mapsterpiece.online:4000/auth',
