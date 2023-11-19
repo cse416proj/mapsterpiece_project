e1661d31cc8361d6b1fa7c03bb86cfdc75442b54
20shiy <shiying6120@gmail.com>
2023-11-19 08:53:26 -0500

    fixed the issue of 'unauthorized'

--- server/routes/store-router.js
+++ server/routes/store-router.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const StoreController = require('../controllers/store-controller');
 const auth = require('../auth');
 
-router.get('/allPosts', auth.verify, StoreController.getAllPosts);
+router.get('/allPosts', StoreController.getAllPosts);
 
 module.exports = router;
