12afea072f7e6e6b71cad9072a5155bf36a508d3
20shiy <shiying6120@gmail.com>
2023-11-19 22:15:39 -0500

    fix error 401 after delete account

--- server/routes/user-router.js
+++ server/routes/user-router.js
@@ -3,7 +3,7 @@ const router = expresss.Router();
 const UserController = require('../controllers/user-controller');
 const auth = require('../auth');
 
-router.delete('/deleteUser/:userId', auth.verify, UserController.deleteUserById);
+router.delete('/deleteUser/:userId', UserController.deleteUserById);
 router.get('/user/:userId', UserController.getUserById);
 
 module.exports = router;
