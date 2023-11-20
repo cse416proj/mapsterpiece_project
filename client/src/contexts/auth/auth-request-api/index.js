45104aa5aa3767e4a3d5dc5c16395a9f3dcae374
20shiy <shiying6120@gmail.com>
2023-11-20 16:48:11 -0500

    changed based url

--- client/src/contexts/auth/auth-request-api/index.js
+++ client/src/contexts/auth/auth-request-api/index.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 axios.defaults.withCredentials = true;
 
 const api = axios.create({
-    // baseURL: 'http://mapsterpiece.online:4000/auth',
-    baseURL: 'http://localhost:4000/auth',
+    baseURL: 'http://mapsterpiece.online:4000/auth',
+    // baseURL: 'http://localhost:4000/auth',
 })
 
 // All requests that will be making
--- client/src/contexts/map/map-request-api/index.js
+++ client/src/contexts/map/map-request-api/index.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 axios.defaults.withCredentials = true;
 
 const api = axios.create({
-  // baseURL: 'http://mapsterpiece.online:4000/map',
-    baseURL: 'http://localhost:4000/map',
+  baseURL: 'http://mapsterpiece.online:4000/map',
+    // baseURL: 'http://localhost:4000/map',
 })
 
 export const createMap = (ownerUserName, title, fileFormat, mapContent, tags) => {
