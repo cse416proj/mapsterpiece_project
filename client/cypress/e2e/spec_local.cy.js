b80bbdd07353487ba900a354c73d9ebcb5575f99
20shiy <shiying6120@gmail.com>
2023-11-27 16:03:37 -0500

    modified workflow order and cypress test

--- client/cypress/e2e/spec_local.cy.js
+++ client/cypress/e2e/spec_local.cy.js
@@ -2,55 +2,55 @@ describe('Mapsterpiece local tests', () => {
   // bob's account (already registered) :
   // bobDD@gmail.com
   // ThisIsAPassword
-  it('register user fail', () => {
-    cy.visit('http://localhost:3000');
-    cy.get('.createAccount').click();
-    cy.url().should('include', '/register');
-    cy.get('input[name = "firstName"]').clear('Bob');
-    cy.get('input[name = "firstName"]').type('Bob');
-    cy.get('input[name = "lastName"]').clear('Dylan');
-    cy.get('input[name = "lastName"]').type('Dylan');
-    cy.get('input[name = "userName"]').clear('BobaKing');
-    cy.get('input[name = "userName"]').type('BobaKing');
-    cy.get('input[name = "email"]').clear('bobDD@gmail.com');
-    cy.get('input[name = "email"]').type('bobDD@gmail.com');
-    cy.get('input[name = "password"]').clear('ThisIsAPassword');
-    cy.get('input[name = "password"]').type('ThisIsAPassword');
-    cy.get('input[name = "confirmPassword"]').clear('ThisIsAnotherPassword');
-    cy.get('input[name = "confirmPassword"]').type('ThisIsAnotherPassword');
-    cy.get('button[type = "submit"]').click();
-    cy.get('#auth-alert').should('be.visible').should('contain', 'Please enter the same password twice.');
-  })
+  // it('register user fail', () => {
+  //   cy.visit('http://localhost:3000');
+  //   cy.get('.createAccount').click();
+  //   cy.url().should('include', '/register');
+  //   cy.get('input[name = "firstName"]').clear('Bob');
+  //   cy.get('input[name = "firstName"]').type('Bob');
+  //   cy.get('input[name = "lastName"]').clear('Dylan');
+  //   cy.get('input[name = "lastName"]').type('Dylan');
+  //   cy.get('input[name = "userName"]').clear('BobaKing');
+  //   cy.get('input[name = "userName"]').type('BobaKing');
+  //   cy.get('input[name = "email"]').clear('bobDD@gmail.com');
+  //   cy.get('input[name = "email"]').type('bobDD@gmail.com');
+  //   cy.get('input[name = "password"]').clear('ThisIsAPassword');
+  //   cy.get('input[name = "password"]').type('ThisIsAPassword');
+  //   cy.get('input[name = "confirmPassword"]').clear('ThisIsAnotherPassword');
+  //   cy.get('input[name = "confirmPassword"]').type('ThisIsAnotherPassword');
+  //   cy.get('button[type = "submit"]').click();
+  //   cy.get('#auth-alert').should('be.visible').should('contain', 'Please enter the same password twice.');
+  // })
 
-  it('login user fail', function() {
-    cy.visit('http://localhost:3000');
-    cy.get('.login').click();
-    cy.url().should('include', '/login');
-    cy.get('input[name = "email"]').clear('bobDD@gmail.com');
-    cy.get('input[name = "email"]').type('bobDD@gmail.com');
-    cy.get('input[name = "password"]').clear('ThisIsAnotherPassword');
-    cy.get('input[name = "password"]').type('ThisIsAnotherPassword');
-    cy.get('button[type = "submit"]').click();
-    cy.get('#auth-alert').should('be.visible').should('contain', 'Wrong email or password provided.');
-  })
+  // it('login user fail', function() {
+  //   cy.visit('http://localhost:3000');
+  //   cy.get('.login').click();
+  //   cy.url().should('include', '/login');
+  //   cy.get('input[name = "email"]').clear('bobDD@gmail.com');
+  //   cy.get('input[name = "email"]').type('bobDD@gmail.com');
+  //   cy.get('input[name = "password"]').clear('ThisIsAnotherPassword');
+  //   cy.get('input[name = "password"]').type('ThisIsAnotherPassword');
+  //   cy.get('button[type = "submit"]').click();
+  //   cy.get('#auth-alert').should('be.visible').should('contain', 'Wrong email or password provided.');
+  // })
 
-  it('login-logout user success', function() {
-    cy.visit('http://localhost:3000');
-    cy.get('.login').click();
-    cy.url().should('include', '/login');
+  // it('login-logout user success', function() {
+  //   cy.visit('http://localhost:3000');
+  //   cy.get('.login').click();
+  //   cy.url().should('include', '/login');
 
-    cy.get('input[name = "email"]').clear('bobDD@gmail.com');
-    cy.get('input[name = "email"]').type('bobDD@gmail.com');
-    cy.get('input[name = "password').clear('ThisIsAPassword');
-    cy.get('input[name = "password').type('ThisIsAPassword');
-    cy.get('button[type = "submit"]').click();
-    cy.wait(1000);
-    cy.get('#auth-alert').should('contain', 'Login success! Now redirecting...')
-    cy.wait(1000);
-    cy.get('#initialsIcon').click();
-    cy.get('.MuiList-root > [tabindex="-1"]').click();
-    cy.get('#logo').click();
-  })
+  //   cy.get('input[name = "email"]').clear('bobDD@gmail.com');
+  //   cy.get('input[name = "email"]').type('bobDD@gmail.com');
+  //   cy.get('input[name = "password').clear('ThisIsAPassword');
+  //   cy.get('input[name = "password').type('ThisIsAPassword');
+  //   cy.get('button[type = "submit"]').click();
+  //   cy.wait(1000);
+  //   cy.get('#auth-alert').should('contain', 'Login success! Now redirecting...')
+  //   cy.wait(1000);
+  //   cy.get('#initialsIcon').click();
+  //   cy.get('.MuiList-root > [tabindex="-1"]').click();
+  //   cy.get('#logo').click();
+  // })
 
   it('dropdown menu test', ()=>{
     cy.visit('http://localhost:3000');
@@ -64,30 +64,30 @@ describe('Mapsterpiece local tests', () => {
     cy.url().should('include', '/');
   })
 
-  it('user create map fail', ()=>{
-    cy.visit('http://localhost:3000');
-    cy.get('.login').click();
-    cy.url().should('include', '/login');
+  // it('user create map fail', ()=>{
+  //   cy.visit('http://localhost:3000');
+  //   cy.get('.login').click();
+  //   cy.url().should('include', '/login');
 
-    cy.get('input[name = "email"]').clear('bobDD@gmail.com');
-    cy.get('input[name = "email"]').type('bobDD@gmail.com');
-    cy.get('input[name = "password').clear('ThisIsAPassword');
-    cy.get('input[name = "password').type('ThisIsAPassword');
-    cy.get('button[type = "submit"]').click();
-    cy.wait(1000);
-    cy.get('#auth-alert').should('contain', 'Login success! Now redirecting...')
-    cy.wait(1000);
+  //   cy.get('input[name = "email"]').clear('bobDD@gmail.com');
+  //   cy.get('input[name = "email"]').type('bobDD@gmail.com');
+  //   cy.get('input[name = "password').clear('ThisIsAPassword');
+  //   cy.get('input[name = "password').type('ThisIsAPassword');
+  //   cy.get('button[type = "submit"]').click();
+  //   cy.wait(1000);
+  //   cy.get('#auth-alert').should('contain', 'Login success! Now redirecting...')
+  //   cy.wait(1000);
 
-    cy.get('.CREATE').click();
-    cy.url().should('include', '/create');
-    cy.wait(1000);
+  //   cy.get('.CREATE').click();
+  //   cy.url().should('include', '/create');
+  //   cy.wait(1000);
 
-    cy.get('#title-input').clear('hello map');
-    cy.get('#title-input').type('hello map');
-    cy.get('#upload-button').click();
-    cy.get('.modal-box')
-    cy.get('#dialog-yes-button').click();
-  })
+  //   cy.get('#title-input').clear('hello map');
+  //   cy.get('#title-input').type('hello map');
+  //   cy.get('#upload-button').click();
+  //   cy.get('.modal-box')
+  //   cy.get('#dialog-yes-button').click();
+  // })
 
 
   // it('user create post fail', ()=>{
